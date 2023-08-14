ef5e5579289d6534bdd079c3aa3f8cdfadf5cd37
Pedro Ladeira <pmladeira@live.com>
2023-08-14 03:55:36 -0300

    hackathon: last commit before submit

--- src/components/chat-message.tsx
+++ src/components/chat-message.tsx
@@ -57,6 +57,7 @@ export default function Message({
             </>
           ) : (
             <>
+              {/* @ts-ignore */}
               <TypeOnce>{message.data}</TypeOnce>
             </>
           )}
